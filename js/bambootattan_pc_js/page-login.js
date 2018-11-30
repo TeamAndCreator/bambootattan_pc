81277dc51b343d3377a77990fdfbf0d8ee7336df
wzljx <2574325794@qq.com>
2018-11-30 22:25:36 +0800

    modify page

--- js/bambootattan_pc_js/page-login.js
+++ js/bambootattan_pc_js/page-login.js
@@ -1,14 +1,14 @@
 function login(){
     //var txt_user_name=document.getElementById("txt_user_name").value;//js
     //var txt_password=document.getElementById("txt_password").value;//js
-    var account_l=$('#account_l').val();//jquery
-    var password_1=$('#password_1').val();//jquery
-    if(account_l=="w" && password_1=="1"){
+    var account=$('#account').val();//jquery
+    var password=$('#password').val();//jquery
+    if(account=="w" && password=="1"){
         // alert("登入成功");
-        window.location="index.html";
+        window.location="home.html";
         // window.event.returnValue=false;
         //window.location.href="main.html";
-        // window.open("index.html");
+        // window.open("home.html");
     }else{
         alert("用户名或密码错误");
     }
