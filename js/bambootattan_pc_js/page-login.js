4140326afd3ac12e5ebe079f130d7aec02de813c
wzljx <2574325794@qq.com>
2018-12-07 22:02:36 +0800

    modify login

--- js/bambootattan_pc_js/page-login.js
+++ js/bambootattan_pc_js/page-login.js
@@ -1,23 +1,45 @@
-function login(){
-    //var txt_user_name=document.getElementById("txt_user_name").value;//js
-    //var txt_password=document.getElementById("txt_password").value;//js
-    var account=$('#account').val();//jquery
-    var password=$('#password').val();//jquery
-    if(account=="w" && password=="1"){
-        // alert("登入成功");
-        window.location="home.html";
-        // window.event.returnValue=false;
-        //window.location.href="main.html";
-        // window.open("home.html");
-    }else{
-        alert("用户名或密码错误");
-    }
-}
-//jquery
 $(function () {
-    $('.submit').on('click',function () {
-        login();
+    //此处为校验的核心代码
+    $("#form-login").bootstrapValidator({
+        submitHandler: function (valiadtor, loginForm, submitButton) {
+
+            valiadtor.defaultSubmit();
+        },
+        fields: {
+            userName: {
+                validators: {
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    stringLength: {
+                        /*长度提示*/
+                        min: 3,
+                        max: 10,
+                        message: '用户名长度必须在3到10之间'
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    stringLength: {
+                        /*长度提示*/
+                        min: 6,
+                        max: 30,
+                        message: '密码长度必须在6到30之间'
+                    },
+                    different: {//不能和用户名相同
+                        field: 'loginName',//需要进行比较的input name值
+                        message: '不能和用户名相同'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_\.]+$/,
+                        message: '密码由数字字母下划线和.组成'
+                    }
+                }
+            },
+        }
     });
 });
-
-
